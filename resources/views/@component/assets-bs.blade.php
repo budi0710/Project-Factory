8307942028683dd46da4479c81a3efb568d43200
lamhotsimamora <lamhotsimamora36@gmail.com>
2025-06-20 12:27:28 +0700

    +

--- resources/views/@component/assets-bs.blade.php
+++ resources/views/@component/assets-bs.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<script src="./storage/vue.js"></script>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
--- resources/views/@component/assets.blade.php
+++ resources/views/@component/assets.blade.php
@@ -1,8 +1,12 @@
-<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/gh/lamhotsimamora/garuda-javascript@master/src/garuda.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/lamhotsimamora/upload-js@master/src/upload.js"></script>
-<script src="./storage/function.js"></script>
+<script src="./vue.js"></script>
+
+<link href="./daisy.ui.css" rel="stylesheet" type="text/css" />
+<script src="./talwind.js"></script>
+<script src="./axios.js"></script>
+
+<script src="./sweetalert.js"></script>
+
+<script src="./garuda.js"></script>
+<script src="./upload.js"></script>
+
+<script src="./function.js"></script>
